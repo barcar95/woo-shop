cc3f4d10b176ac5e8550fd9ae0195a27da18627a
barre <barrenac@oregonstate.edu>
2023-08-07 15:51:08 -0700

    Added missing objects to and finished API routes

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -3,26 +3,67 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async(req, res) => {
   // find all categories
+  try {
+    const categoryData = await Category.findAll({ 
+      include: [{model: Product}]
+    })
+    res.status(200).json(categoryData);
+  } catch (error) {
+    res.status(500).json(error);
+  }
   // be sure to include its associated Products
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async(req, res) => {
   // find one category by its `id` value
+  try {
+    const singleCategory = await Category.findByPk(req.params.id, {
+      include: [{ model: Product}]
+    })
+    if(!singleCategory){
+      return res.status(404).json({message: 'Category Not Found'})
+    }
+    res.status(200).json(singleCategory);
+  } catch (error) {
+    res.status(500).json(error);
+  }
   // be sure to include its associated Products
 });
 
-router.post('/', (req, res) => {
+router.post('/', async(req, res) => {
   // create a new category
+  try {
+    const createCat = await Category.create(req.body)
+    res.status(200).json(createCat);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async(req, res) => {
   // update a category by its `id` value
+  try {
+    const updateCat = await Category.update(req.body, {
+      where: {id: req.params.id}
+    });
+    res.status(200).json(updateCat);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async(req, res) => {
   // delete a category by its `id` value
+  try {
+    const deleteCat = await Category.destroy({
+      where: {id: req.params.id}
+    });
+    res.status(200).json({message: `${deleteCat} categories deleted.`})
+  } catch (error) {
+    res.status(500).json(error)
+  }
 });
 
 module.exports = router;
--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -4,19 +4,37 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async(req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  try {
+    const prodData = await Product.findAll({
+      include: [{model: Category}, {model: Tag}]
+    })
+    res.status(200).json(prodData);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async(req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    const singleProd = await Product.findByPk(req.params.id, {
+      include: [{ model: Category}, {model: Tag}]
+    })
+    if(!singleProd){
+      return res.status(404).json({message: 'Product Not Found.'})
+    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
 // create new product
-router.post('/', (req, res) => {
+router.post('/', async(req, res) => {
   /* req.body should look like this...
     {
       product_name: "Basketball",
@@ -25,6 +43,12 @@ router.post('/', (req, res) => {
       tagIds: [1, 2, 3, 4]
     }
   */
+ try {
+  const createProd = await Product.create(req.body)
+  res.status(200).json(createProd);
+ } catch (error) {
+  res.status(500).json(error);
+ }
   Product.create(req.body)
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
@@ -92,8 +116,16 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async(req, res) => {
   // delete one product by its `id` value
+  try {
+    const deleteProd = await Product.destroy({
+      where: {id: req.params.id}
+    });
+    res.status(200).json({message: `${deleteProd} products deleted.`})
+  } catch (error) {
+    res.status(500).json(error)
+  }
 });
 
 module.exports = router;
